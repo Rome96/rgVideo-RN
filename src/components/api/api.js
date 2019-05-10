1488bcb18476dec58ac0283ee24198d1359f20c6
Turiano Romero <romero.guzman18@gmail.com>
2019-05-10 13:24:30 -0500

    consumiendo datos del API :grinning:

---
+++ src/components/api/api.js
@@ -0,0 +1,11 @@
+const host = 'https://yts.am/api/v2'
+
+class Api { 
+    async getSuggestion(id){
+        const query = await fetch(`${host}/movie_suggestions.json?movie_id=${id}`)
+        const {data} = await query.json()
+        return data.movies
+    }
+}
+export default new Api()
+
